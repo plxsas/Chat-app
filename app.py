cf19632ba5d6fde5cf4adf88944760454eb9ad0d
plxsas <sale12302006@yahoo.com>
2020-07-10 13:41:40 +0100

    Add chat textbox function ability and refactored code

--- app.py
+++ app.py
@@ -1,22 +1,43 @@
 import os
-from flask import Flask
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
+
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
+messages = []
 
 
-@app.route('/')
+def add_message(username, message):
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append({"timestamp": now, "from": username, "message": message})
+ 
+@app.route("/", methods=["GET", "POST"])
 def index():
-    """ Main page with instructions """
-    return "To send a message use /USERNAME/MESSAGE"
+    """Main page with instructions"""
+    if request.method == "POST":
+        session["username"] = request.form["username"]
 
-@app.route('/<username>')
+    if "username" in session:
+        return redirect(url_for("user", username = session["username"]))
+
+    return render_template("index.html")
+
+
+@app.route("/chat/<username>", methods=["GET", "POST"])
 def user(username):
-    return "Hi " + username
+    """Add and display chat messages"""
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(url_for("user", username = session["username"]))
+
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
 
 
-@app.route('/<username>/<message>')
-def send_message(username, message):
-    return "{0}: {1}".format(username, message)
 
 
 if __name__ =="__main__":
